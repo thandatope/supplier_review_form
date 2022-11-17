402a5805ca24bc9ef3d7d7d1f5f5e2dcfa99f071
Jedd Marston <104775158+hippopotamusicetea@users.noreply.github.com>
2022-11-17 09:25:28 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,29 +1,3 @@
-"""
-
-Todo:
-Docstrings
-Clean up
-Protect email details etc once have a proper account and not a rubbish disposable gmail
-Improve report emails - yagmail has better formatting options
-Wider reporting emails
-Update this to reflect the structure required for pythonanywhere so can just zip and upload
-Have email kept in session automatically entered into forms w/ disabled boxes <- Low prio, probably dont bother
-Same w/ date
-    Email/date can also just be hidden fields
-
-Styling out of HTML files and into static CSS files within blueprint dirs
-Uploaded files stored in memory as bytesIO instead of saving to disk
-
-PDF reports: https://github.com/Edinburgh-Genome-Foundry/pdf_reports
-Notification width lower
-
-https://pypi.org/project/flask-bigapp/
-Remove text from cards, only have items in it?
-add validators to forms, mandatory fields etc
-use tempfile.gettempdir() etc for temp file storage
-
-"""
-
 from app import create_app
 
 app = create_app()
